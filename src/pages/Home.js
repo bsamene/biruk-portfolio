2b3e3ee74f52028f3bc56535d1a8d5726d485c30
Biruk Amene <bsa16@duke.edu>
2023-08-22 21:06:35 -0400

    typewriter effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,17 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import "../styles/Home.css"
+import Typewriter from "typewriter-effect";
 function Home() {
   return <div className = "home">
     <div className = "about">
-     <h2> Biruk Amene </h2>
+     <h2>
+      <Typewriter
+      onInit = {(typewriter) => {
+        typewriter.typeString("Biruk Amene").start();
+      }}
+      />
+     </h2>
      <div className = "prompt">
         <p> CS @ Duke University</p>
         <InstagramIcon />
