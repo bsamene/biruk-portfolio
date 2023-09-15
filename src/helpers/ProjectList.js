9d082b46701a811bd767eff0ea7303a908504632
Biruk Amene <bsa16@duke.edu>
2023-09-15 15:49:00 -0400

    finishing up ecostart

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,9 +1,7 @@
 import Placeholder1 from "../assets/Placeholder1.png"
 import Placeholder2 from "../assets/Placeholder2.png"
 import Placeholder3 from "../assets/Placeholder3.png"
-import Placeholder4 from "../assets/Placeholder4.png"
-import Placeholder5 from "../assets/Placeholder5.png"
-import Placeholder6 from "../assets/Placeholder6.png"
+
 
 export const ProjectList = [
     {
@@ -19,21 +17,6 @@ export const ProjectList = [
     {
         name: "Type Racer",
         image: Placeholder3,
-        skills: "Javascript, HTML, CSS",
-    },
-    {
-        name: "Type Racer",
-        image: Placeholder4,
-        skills: "Javascript, HTML, CSS",
-    },
-    {
-        name: "Type Racer",
-        image: Placeholder5,
-        skills: "Javascript, HTML, CSS",
-    },
-    {
-        name: "Type Racer",
-        image: Placeholder6,
         skills: "Javascript, HTML, CSS",
     }
 ]
