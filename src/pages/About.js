0ba4780df9a07249522f1a39c296515b639dd544
Biruk Amene <bsa16@duke.edu>
2023-09-16 20:16:34 -0400

    finishing up About me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "../styles/About.css"
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <h1 className = "aboutTitle">About Me</h1>
+    <div className = "aboutDescription">
+
+        <p>Hi there! My name is Biruk Amene. I'm currently studying computer science at Duke University, concentrating in software systems.</p>
+
+    </div>
+    </>
   )
 }
 
