eefe08a651bfe3f8f86c1d683e1985e5e81724a9
Biruk Amene <bsa16@duke.edu>
2023-09-16 22:48:16 -0400

    wrapping up about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../styles/About.css"
-
+import AboutUsImage from "../assets/AboutUsImage.JPG"
 function About() {
   return (
     <>
@@ -8,7 +8,7 @@ function About() {
     <div className = "aboutDescription">
 
         <p>Hi there! My name is Biruk Amene. I'm currently studying computer science at Duke University, concentrating in software systems. When I'm not programming, you'll catch me playing basketball, NYT Games, watching One Piece, and doing escape rooms. This site highlights some of my computer science related ventures during my time at Duke, feel free to explore!</p>
-
+        <img src={AboutUsImage} alt="About Me" className="aboutImage" />
     </div>
     </>
   )
