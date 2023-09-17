3f8b3212fa9ef75ee53dae52f9271d0100dac39f
Biruk Amene <bsa16@duke.edu>
2023-09-16 20:33:43 -0400

    about me description done

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
     <h1 className = "aboutTitle">About Me</h1>
     <div className = "aboutDescription">
 
-        <p>Hi there! My name is Biruk Amene. I'm currently studying computer science at Duke University, concentrating in software systems.</p>
+        <p>Hi there! My name is Biruk Amene. I'm currently studying computer science at Duke University, concentrating in software systems. When I'm not programming, you'll catch me playing basketball, NYT Games, watching One Piece, and doing escape rooms. This site highlights some of my computer science related ventures during my time at Duke, feel free to explore!</p>
 
     </div>
     </>
