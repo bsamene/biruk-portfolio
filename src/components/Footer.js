f9077587d4c16eb380456aa80cc3f791cef3cd47
Biruk Amene <bsa16@duke.edu>
2024-05-05 23:51:36 -0400

    updated picture and copyright date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div className="socialMedia">
 
     </div>
-        <p className = "copyright"> &copy; 2023 Biruk Amene</p>
+        <p className = "copyright"> &copy; 2024 Biruk Amene</p>
     </div>
   );
 }
